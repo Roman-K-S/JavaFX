da4c8a1c8ed5a9ea9a97caa201a9ec47119277f0
Roman-K-S <70799864+Roman-K-S@users.noreply.github.com>
2021-10-25 22:35:15 +0300

    Lesson_2_4

--- src/main/java/com/example/javafx/HelloController.java
+++ src/main/java/com/example/javafx/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    protected void onHelloButtonClick() {
-//          welcomeText.setText("Welcome to JavaFX Application!");
+    TextArea chatText;
+    @FXML
+    TextField inputText;
+
+    @FXML
+    protected void onHelloButtonClick(ActionEvent actionEvent) {
+        chatText.appendText(inputText.getText() + "\n" );
     }
 }
